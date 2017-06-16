598e1503cff27f50f95d05e0c998080b372a6fd0
GBardis <crisolivasavatage@hotmail.com>
2017-06-16 14:46:17 +0300

    add spotify api

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,13 +1,63 @@
 class ArtistsController < ApplicationController
-before_action do
+  before_action do
     @discogs = Discogs::Wrapper.new("Test OAuth", user_token: 'uUAcpNNqtCmvrLITnpzKeuxfUcxsQdeGRJRggXqq')
   end
+  before_action do
+    RSpotify.authenticate('11180b801be74a9a9181777e6f78d5d9', '0e4a3494142b4b24b01f487b3ae6631d')
 
+  end
 
- def show
-  @artist = Artist.find(params[:id])
-  @result =  @discogs.search(@artist.name.to_s, :per_page => 10, :type => :artist)
-  @artist_discogs = @discogs.get_artist(@result.results.first.id.to_s)
+  def show
+    @artist = Artist.find(params[:id])
+    @result =  @discogs.search(@artist.name.to_s, :per_page => 10, :type => :artist)
+    @artist_discogs = @discogs.get_artist(@result.results.first.id.to_s)
 
- end
+  end
+  def test
+    # @result =  @discogs.search("metallica", :per_page => 10, :type => :artist)
+    # @artist_discogs = @discogs.get_artist(@result.results.first.id.to_s)
+    #
+    # @saveband = Band.new(
+    #   name: @artist_discogs.name,
+    #   description: @artist_discogs.profile
+    # )
+    # @saveband.save
+    #
+    # @artist_discogs.members.each do |member|
+    #   @result =  @discogs.search(member.name.to_s, :per_page => 10, :type => :artist)
+    #   @artist_discogs = @discogs.get_artist(@result.results.first.id.to_s)
+    #   @saveart = Artist.new(
+    #     name: member.name,
+    #     description: @artist_discogs.profile
+    #   )
+    #   @saveart.save
+    # end
+    # byebug
+    #  @genres = @arcticmonkeys.genres
+    #@albums = @arcticmonkeys.albums
+    #byebug
+
+    #  @result =  @discogs.search("metallica",:per_page => 100, :type => :artist)
+    #@artist_discogs_songs = @discogs.get_artist_releases(@result.results.first.id.to_s)
+    # @artist = Band.find_by(name: "Metallica")
+    # @artist_discogs_songs.releases.each do |release|
+    #   @song = Song.new(
+    #     artist_id: @artist.id,
+    #   name: release.title)
+    # end
+    ######NEW SCRIPT#############
+    # @result =  @discogs.search("Metallica", :per_page => 10, :type => :artist)
+    # @artist_discogs = @discogs.get_artist(@result.results.first.id.to_s)
+    #
+    # @saveband = Band.new(
+    #   name: @artist_discogs.name,
+    #   description: @artist_discogs.profile
+    # )
+    # @saveband.save
+    @artist = RSpotify::Artist.search('Metallica').first
+    # @artist.albums.each do |album|
+    #   album.name
+    #   album.release_date
+    # end
+      end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -46,5 +46,5 @@ Rails.application.routes.draw do
 
   #resources :categories
 
-
+  get 'test' => 'artists#test'
 end
