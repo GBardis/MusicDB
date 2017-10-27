c9355a7f3c46a519c2cd4b8dbda573d04194d798
GBardis <crisolivasavatage@hotmail.com>
2017-10-27 12:33:30 +0300

    fix issue with admin bands not rendering band index , fix issue with admin user not showing new form and missing action create user form admin panel

--- app/controllers/admin/bands_controller.rb
+++ app/controllers/admin/bands_controller.rb
@@ -1,13 +1,13 @@
 class Admin::BandsController < AdminController
   include PhotosHelper
-  before_action :set_band, only:[:update,:destroy]
+  before_action :set_band, only: %i[update destroy]
   def index_band
     @bands = Band.all
   end
 
   def new_band
     @band = Band.new
-    @artists = Artist.all.map{|x| ["#{x.name}" ,"#{x.id}"]}
+    @artists = Artist.all.map { |x| [x.name.to_s, x.id.to_s] }
   end
 
   def create
@@ -15,8 +15,8 @@ class Admin::BandsController < AdminController
     if @band.save.dig(:artist)
       if params[:band][:artists]
         params[:band][:artists].each do |artist|
-          @bandmembers = @band.bandmembers.build(artist_id: artist) #unless artist.empty?
-          @bandmembers.save #unless artist.empty?
+          @bandmembers = @band.bandmembers.build(artist_id: artist) # unless artist.empty?
+          @bandmembers.save # unless artist.empty?
         end
       end
       if params[:images]
@@ -27,22 +27,22 @@ class Admin::BandsController < AdminController
         @band.photos.create
       end
       respond_to do |format|
-        format.html{render partial: 'bands/new'}
+        format.html { render partial: 'bands/new' }
       end
       flash[:success] = "Band #{@band.name} is created"
       redirect_to admin_bands_path
     else
-      flash[:warning] = "Please try again"
+      flash[:warning] = 'Please try again'
       redirect_to admin_bands_new_path
     end
   end
 
   def edit_band
     @band = Band.find(params[:id])
-    @artists = Artist.all.map{|x| ["#{x.name}" ,"#{x.id}"]}
+    @artists = Artist.all.map { |x| [x.name.to_s, x.id.to_s] }
 
     @count = Bandmember.where(band_id: @band.id)
-    #byebug
+    # byebug
     @array = []
     if @count.size > 1
       @count.each do |artist|
@@ -54,25 +54,25 @@ class Admin::BandsController < AdminController
       @artist = Artist.find(@count.artist_id).id
       @array << @artist
     else
-      @array = [""]
+      @array = ['']
     end
-    #@array = [""]
-    #byebug
+    # @array = [""]
+    # byebug
   end
-  #@selected_artists = @array.map{|x| ["#{x.name}" ,"#{x.id}"]}
+
+  # @selected_artists = @array.map{|x| ["#{x.name}" ,"#{x.id}"]}
   def update
-    #byebug@band
+    # byebug@band
     if @band.update_attributes(band_params)
       if params[:band][:artists]
         params[:band][:artists].each do |artist|
-          @bandmembers = @band.bandmembers.build(artist_id: artist) #unless artist.empty?
-          @bandmembers.save #unless artist.empty?
+          @bandmembers = @band.bandmembers.build(artist_id: artist) # unless artist.empty?
+          @bandmembers.save # unless artist.empty?
           ## byebug
         end
       end
-      flash[:success] = "good"
+      flash[:success] = 'good'
       redirect_to admin_bands_edit_path
-    else
     end
   end
 
@@ -88,6 +88,6 @@ class Admin::BandsController < AdminController
   end
 
   def band_params
-    params.require(:band).permit(:name,:genre,:description,:artist,:photos,:category_id,artists:[:id])#,bandmember:[:band_id,:artist_id])
+    params.require(:band).permit(:name, :genre, :description, :artist, :photos, :category_id, artists: [:id]) # ,bandmember:[:band_id,:artist_id])
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,4 @@
 class UsersController < ApplicationController
-
   def new
     @user = User.new
   end
@@ -8,15 +7,13 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
-  def index
-
-  end
+  def index; end
 
   def create
     @user = User.new(user_params)
     if @user.save
       log_in @user
-      flash[:success] = "Welcome to MusicDB!"
+      flash[:success] = 'Welcome to MusicDB!'
       redirect_to @user
 
     else
@@ -25,7 +22,7 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user=User.find(params[:id])
+    @user = User.find(params[:id])
   end
 
   def update
@@ -38,10 +35,9 @@ class UsersController < ApplicationController
     end
   end
 
-
   private
 
   def user_params
-    params.require(:user).permit(:username,:email,:password,:password_confirmation,:is_admin,:remember_digest)
+    params.require(:user).permit(:username, :email, :password, :password_confirmation, :is_admin, :remember_digest)
   end
 end
